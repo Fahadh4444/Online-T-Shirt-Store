6770ae018d08f335038f3fa96ce03629e835680f
Kasala Fahadh <67508953+Fahadh4444@users.noreply.github.com>
2020-12-25 18:44:33 +0530

    Updated app.js
    Updated using express by creating local port and connected to Database.
    Added dotenv file also to repo

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,2 +1,40 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const authRoutes = require("./routes/auth");
+
+
+//DB CONNECTION
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("DB CONNECTED");
+});
+
+
+//MIDDLEWARES
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+//MY ROUTES
+app.use("/api", authRoutes);
+
+
+//PORT
+const port = process.env.PORT || 8000;
+
+
+//STARTING SERVER
+app.listen(port , ()=>{
+    console.log(`App is running at ${port}`);
+});
