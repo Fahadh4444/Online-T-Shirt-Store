1270cbf8a79a0838714fd9633aac7c5d918a3098
Kasala Fahadh <67508953+Fahadh4444@users.noreply.github.com>
2020-12-26 12:46:55 +0530

    Updated auth.js in controllers and routes

---
+++ projbackend/controllers/auth.js
@@ -0,0 +1,15 @@
+
+
+exports.signup = (req,res) => {
+    console.log("REQ BODY", req.body);
+    res.json({
+        message: "signup route works!"
+    });
+};
+
+
+exports.signout = (req, res) => {
+    res.json({
+        message: "User Signout"
+    });
+};
