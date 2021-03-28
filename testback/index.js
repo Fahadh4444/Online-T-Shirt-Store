e05ac39ef3ee7a8a2c14e5d1daa8526f5e6dff4a
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-03-28 21:02:30 +0530

    Updated index.js

--- testback/index.js
+++ testback/index.js
@@ -4,22 +4,20 @@ const app = express();
 
 const port = 3030;
 
-const admin = (req, res) => {
-    return res.send("This is Admin")
-};
-
 
 app.get("/", (req, res) => {
     return res.send("You are here finally!!!");
 });
 
 
+const admin = (req, res) => {
+    return res.send("This is Admin")
+};
 const isAdmin = (req, res, next) => {
     console.log("isAdmin is running");
     next();
 };
-
-app.get("/admin", isAdmin, admin);//We can craft code that if isAdmin is true then only next() will be returned otherwise, we stop the request.
+app.get("/admin", isAdmin, admin);   //? We can craft code that if isAdmin is true then only next() will be returned otherwise, we stop the request.
 
 
 app.get("/login", (req, res) => {
