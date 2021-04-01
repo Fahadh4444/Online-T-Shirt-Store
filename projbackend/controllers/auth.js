2764efa5318aee49a856db77c8fa2d44c89b15b7
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-01 23:50:35 +0530

    Created signin route in controllers.
    For this I used jsonwebtoken and express-jwt packages....

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,5 +1,8 @@
 const User = require("../models/user");
 const { check, validationResult } = require('express-validator');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
+
 
 exports.signup = (req, res) => {
 
@@ -23,6 +26,41 @@ exports.signup = (req, res) => {
             id: user._id
         });
     });
+};
+
+
+exports.signin = (req,res) => {
+    const { email, password } = req.body;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            error: errors.array()[0].msg
+        });
+    };
+
+    User.findOne({email}, (err, user) => {
+        if(err){
+            res.status(400).json({
+                error: "User does not exists"
+            });
+        };
+
+        if(!user.autheticate(password)){
+            res.status(401).json({
+                error: "Email and Password do not match"
+            });
+        };
+
+        //? Create TOKEN by MAJIDH
+        const token = jwt.sign({_id: user._id}, process.env.SECRET);
+        //? Put token in cookie
+        res.cookie("token", token, {expire: new Date() + 9999});
+        //? Send response to front end
+        const {_id, name, email, role} = user;
+        return res.json({token, user: { _id, name, email, role}});
+    });
+
 };
 
 
