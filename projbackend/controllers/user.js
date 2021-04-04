0d0bb19d4effcc387ee3b7391bc9fa23bdd48837
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-04 23:57:17 +0530

    Removed getAllUsers Route's callback function in controllers to get all users data from DB(Assignment) from main code.

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -21,15 +21,3 @@ exports.getUser = (req, res) => {
     req.profile.updatedAt = undefined;
     return res.json(req.profile);
 }
-
-//* getAllUsers Route
-exports.getAllUsers = (req, res) => {
-    User.find().exec((err, users) => {
-        if (err || !users) {
-            return res.status(400).json({
-                error: "NO USERS FOUND"
-            });
-        };
-        res.json(users);
-    })
-}
