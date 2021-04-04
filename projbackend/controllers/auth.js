07b46ae86afcc8d1734d297e3440ebe574330f1c
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-04 23:10:01 +0530

    Modified auth.js in controller.
    Changed === to ==.

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -28,9 +28,8 @@ exports.signup = (req, res) => {
     });
 };
 
-
 //* SINGNIN
-exports.signin = (req,res) => {
+exports.signin = (req, res) => {
     const { email, password } = req.body;
     //? Checking Error
     const errors = validationResult(req);
@@ -41,31 +40,30 @@ exports.signin = (req,res) => {
     };
     //? Fetching user from database using findone
     //? checking email
-    User.findOne({email}, (err, user) => {
-        if(err || !user){
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
             return res.status(400).json({
                 error: "User does not exists"
             });
         };
         // console.log(user.autheticate(password));
         //? Checking Password
-        if(!user.autheticate(password)){
+        if (!user.autheticate(password)) {
             return res.status(401).json({
                 error: "Email and Password do not match"
             });
         };
         // console.log(user.securePassword(password));
         //? Create TOKEN by MAJIDH
-        const token = jwt.sign({_id: user._id}, process.env.SECRET);
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET);
         //? Put token in cookie
-        res.cookie("token", token, {expire: new Date() + 9999});
+        res.cookie("token", token, { expire: new Date() + 9999 });
         //? Send response to front end
-        const {_id, name, email, role} = user;
-        return res.json({token, user: { _id, name, email, role}});
+        const { _id, name, email, role } = user;
+        return res.json({ token, user: { _id, name, email, role } });
     });
 
 };
-
 
 //* SIGNOUT
 exports.signout = (req, res) => {
@@ -76,18 +74,16 @@ exports.signout = (req, res) => {
     });
 };
 
-
 //* PROTECTED ROUTES
 exports.isSignedIn = expressJwt({
     secret: process.env.SECRET,
     userProperty: "auth"
 });
 
-
 //* CUSTOM MIDDLEWARES
 exports.isAuthenticated = (req, res, next) => {
-    let checker = req.profile && req.auth && req.profile._id === req.auth._id;
-    if(!checker){
+    let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+    if (!checker) {
         return res.status(403).json({
             error: "ACCESS DENIED"
         });
@@ -95,7 +91,7 @@ exports.isAuthenticated = (req, res, next) => {
     next();
 };
 exports.isAdmin = (req, res, next) => {
-    if(req.profile.role === 0){
+    if (req.profile.role === 0) {
         return res.status(403).json({
             error: "You are not ADMIN, Access Denied"
         });
