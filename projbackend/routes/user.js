f447b110fce110f33fc33993be82ea26bc7dab64
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-04 23:57:08 +0530

    Removed getAllUsers Route to get all users data from DB(Assignment) from main code.

--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserById, getUser, getAllUsers } = require("../controllers/user");
+const { getUserById, getUser } = require("../controllers/user");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
 //* Middleware
@@ -10,7 +10,5 @@ router.param("userId", getUserById);
 //* getUser post route
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 
-//* getAllUsers post route
-router.get("/users", getAllUsers);
 
 module.exports = router;
