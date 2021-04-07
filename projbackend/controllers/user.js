85f313079dc852450a9adf03d596db94c577457c
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-07 16:11:15 +0530

    Created updateUser and userPurchaseList method also pushOrderInPurchaseList  middleware.

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const Order = require("../models/order");
 
 //* Middleware
 exports.getUserById = (req, res, next, id) => {
@@ -21,3 +22,68 @@ exports.getUser = (req, res) => {
     req.profile.updatedAt = undefined;
     return res.json(req.profile);
 }
+
+//* updateUser Route
+exports.updateUser = (req, res) => {
+    User.findByIdAndUpdate(
+        { _id: req.profile._id },
+        { $set: req.body },
+        { new: true, useFindAndModify: false },
+        (err, user) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "You are not authorized to update thsi user"
+                });
+            };
+            user.salt = undefined;
+            user.encry_password = undefined;
+            user.createdAt = undefined;
+            user.updatedAt = undefined;
+            return res.json(user);
+        }
+    );
+}
+
+//* userPurchaseList Route
+exports.userPurchaseList = (req, res) => {
+    Order.find({ user: req.profile._id })
+        .populate("user", "_id name")
+        .exec((err, order) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "No Orders found for this accout"
+                });
+            }
+            return res.json(order);
+        })
+}
+
+//* MiddleWare
+exports.pushOrderInPurchaseList = (req, res, next) => {
+    let purchases = []
+    req.body.order.products.forEach(product => {
+        purchases.push({
+            _id: product._id,
+            name: product.name,
+            description: product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount: req.body.order.amount,
+            transaction_id: req.body.order.transaction_id
+        });
+    });
+    //? Storing in DB
+    User.findOneAndUpdate(
+        { _id: req.profile._id },
+        { $push: { purchases: purchases } },
+        { new: true },
+        (err, purchases) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Unable to Purchase List"
+                });
+            }
+            next();
+        }
+    );
+};
