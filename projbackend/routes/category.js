88c9431000d7624f307b7a1650b00ce3771befa5
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-08 22:21:23 +0530

    Created updateCategory and removeCategory Routes using getCategoryById and getUserId Params.

--- projbackend/routes/category.js
+++ projbackend/routes/category.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { getCategoryById, createCategory, getCategory, getAllCategories } = require("../controllers/category");
+const { getCategoryById, createCategory, getCategory, getAllCategories, updateCategory, removeCategory } = require("../controllers/category");
 const { isAuthenticated, isAdmin, isSignedIn } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
 //* (Params)Middlewares
 router.param("userId", getUserById);
-router.param("catergoryId", getCategoryById);
+router.param("categoryId", getCategoryById);
 
 //* createCategory POST Route
 router.post("/category/create/:userId", isSignedIn, isAuthenticated, isAdmin, createCategory);
@@ -15,5 +15,9 @@ router.post("/category/create/:userId", isSignedIn, isAuthenticated, isAdmin, cr
 router.get("/category/:categoryId", getCategory);
 //* getAllCategories GET Route
 router.get("/categories", getAllCategories);
+//* updateCategory PUT Route
+router.put("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, updateCategory);
+//* removeCategory DELETE Route
+router.delete("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, removeCategory);
 
 module.exports = router;
