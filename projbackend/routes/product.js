59135e1a795ca850214dac87e5a1ef32a5430378
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-12 01:33:41 +0530

    Created routes which gets details of products.

--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProductById, createProduct } = require("../controllers/product")
+const { getProductById, createProduct, getProduct, photo } = require("../controllers/product")
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
@@ -11,5 +11,8 @@ router.param("productId", getProductById);
 
 //* createProduct POST Route
 router.post("/product/create/:userId", isSignedIn, isAuthenticated, isAdmin, createProduct);
-
+//* getProduct GET Route
+router.get("/product/:productId", getProduct);
+//* Middleware Route to load photo in background
+router.get("/product/photo/:productId", photo);
 module.exports = router;
