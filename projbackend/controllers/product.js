e0d42becfeabd59923c8898abc366f096b398410
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-13 20:30:55 +0530

    Modified product.js in controllers by adding deleteProduct, updateProduct, getAllProducts route methods and updateStock middleware.

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -81,3 +81,97 @@ exports.photo = (req, res, next) => {
     };
     next();
 }
+
+//* deleteProduct Route Method
+exports.deleteProduct = (req, res) => {
+    let product = req.product;
+    product.remove((err, deletedProduct) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Failed to delete the product"
+            });
+        }
+        res.json({
+            message: "Deletion was seccessfull",
+            deletedProduct
+        });
+    });
+}
+
+//* updateProduct Route Method
+exports.updateProduct = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+
+    form.parse(req, (err, fields, file) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Problem with Image"
+            });
+        }
+
+        //* Updation File
+        let product = req.product;
+        product = _.extend(product, fields);
+
+        if (file.photo) {
+            if (file.photo.size > 3000000) {
+                return res.status(400).json({
+                    error: "File Size too Big!"
+                });
+            }
+            product.photo.data = fileSystem.readFileSync(file.photo.path);
+            product.photo.contentType = file.photo.type
+        };
+
+        //*Saving to DB
+        product.save((err, product) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Updating Product in DB failed"
+                });
+            }
+            res.json(product);
+        });
+    });
+}
+
+//* getAllProducts Route Method
+exports.getAllProducts = (req, res) => {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 9;
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    Product.find()
+        .select("-photo")
+        .populate("category")
+        .sort([[sortBy, "asc"]])
+        .limit(limit)
+        .exec((err, products) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "No Product Found"
+                });
+            };
+            res.json(products);
+        });
+}
+
+//* Middleware to update stock and sold in productmodel
+exports.updateStock = (req, res, next) => {
+    let myOperations = req.body.order.products.map(product => {
+        return {
+            updateOne: {
+                filter: { _id: product._id },
+                update: { $inc: { stock: -product.count, sold: +product.count } }
+            }
+        }
+    })
+
+    Product.bulkWrite(myOperations, {}, (err, products) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Bulk operation failed !!!"
+            });
+        };
+        next();
+    });
+};
