0bd9bc8e264f5364a93574417afcc943ea950865
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-15 20:55:28 +0530

    Modified product.js in routes by adding getOrderStatus and updateStatus route methods.

--- projbackend/controllers/order.js
+++ projbackend/controllers/order.js
@@ -42,3 +42,24 @@ exports.getAllOrders = (req, res) => {
         res.json(order);
     });
 };
+
+//* getOrderStatus Route Method
+exports.getOrderStatus = (req,res) => {
+    res.json(Order.schema.path("status").enumValues);
+};
+
+//* updateStatus Route Method
+exports.updateStatus = (req,res) => {
+    Order.update(
+        {_id: req.body.orderId},
+        {$set: {status: req.body.status}},
+        (err, order) => {
+            if(err){
+                return res.status(400).json({
+                    error: "Cannot Update Order Status"
+                })
+            };
+            res.json(order);
+        };
+    );
+};
