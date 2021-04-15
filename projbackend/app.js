c08c2253139275663ab50e101a9d06a4e17436c3
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-15 19:46:15 +0530

    Updated app.js for order routes.

--- projbackend/app.js
+++ projbackend/app.js
@@ -14,6 +14,8 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+
 
 
 
@@ -40,6 +42,8 @@ app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+
 
 
 
