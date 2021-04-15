50cbe2db09190dace19475fd44e97e67901b8cd3
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-15 20:55:55 +0530

    Modified product.js in controllers by adding getOrderStatus and updateStatus routes.

--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -5,7 +5,7 @@ const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
 const { updateStock } = require("../controllers/product");
 
-const { getOrderById, createOrder, getAllOrders } = require("../controllers/order");
+const { getOrderById, createOrder, getAllOrders, getOrderStatus, updateStatus } = require("../controllers/order");
 
 //* (Params)Middlewares
 router.param("userId", getUserById);
@@ -15,7 +15,9 @@ router.param("orderId", getOrderById);
 router.post("/order/create/:userId", isSignedIn, isAuthenticated, pushOrderInPurchaseList, updateStock, createOrder);
 //* getAllOrders GET Route
 router.get("/order/all/:userId", isSignedIn, isAuthenticated, isAdmin, getAllOrders);
-
-
+//* getOrderStatus GET Route
+router.get("/order/status/:userId", isSignedIn, isAuthenticated, isAdmin, getOrderStatus);
+//* updateStatus PUT Route
+router.put("/order/:orderId/status/:userId", isSignedIn, isAuthenticated, isAdmin, updateStatus);
 
 module.exports = router;
