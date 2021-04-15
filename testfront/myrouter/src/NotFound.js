6576de470a2723092e5038bc9d5057760e9dfadb
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-16 00:12:02 +0530

    Route Components.

---
+++ testfront/myrouter/src/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App(){
+    return(
+        <div>
+            <h1>
+                404 Page Not Found Component
+            </h1>
+        </div>
+    )
+}
