82249908636523a0d095641c9e7987d8e53ffe79
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-19 00:09:48 +0530

    Added Indentations.

--- projbackend/controllers/order.js
+++ projbackend/controllers/order.js
@@ -3,16 +3,16 @@ const { Order, ProductCart } = require("../models/order");
 //* Middleware
 exports.getOrderById = (req, res, next, id) => {
     Order.findById(id)
-    .populate("products.product", "name price") //! Don't put comma
-    .exec((err, order) => {
-        if(err){
-            return res.status(400).json({
-                error: "No Order found in DB"
-            });
-        };
-        req.order = order;
-        next();
-    })
+        .populate("products.product", "name price") //! Don't put comma
+        .exec((err, order) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "No Order found in DB"
+                });
+            };
+            req.order = order;
+            next();
+        })
 }
 
 //* createOrder Route Method
@@ -20,7 +20,7 @@ exports.createOrder = (req, res) => {
     req.body.order.user = req.profile;
     const order = new Order(req.body.order);
     order.save((err, order) => {
-        if(err){
+        if (err) {
             return res.status(400).json({
                 erroe: "Failed to save your order in DB"
             });
@@ -32,34 +32,34 @@ exports.createOrder = (req, res) => {
 //* getAllOrders Route Method
 exports.getAllOrders = (req, res) => {
     Order.findById()
-    .populate("user", "_id name")
-    .exec((err, order) => {
-        if(err){
-            return res.status(400).json({
-                error: "No Orders found in DB"
-            });
-        }
-        res.json(order);
-    });
+        .populate("user", "_id name")
+        .exec((err, order) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "No Orders found in DB"
+                });
+            }
+            res.json(order);
+        });
 };
 
 //* getOrderStatus Route Method
-exports.getOrderStatus = (req,res) => {
+exports.getOrderStatus = (req, res) => {
     res.json(Order.schema.path("status").enumValues);
 };
 
 //* updateStatus Route Method
-exports.updateStatus = (req,res) => {
+exports.updateStatus = (req, res) => {
     Order.update(
-        {_id: req.body.orderId},
-        {$set: {status: req.body.status}},
+        { _id: req.body.orderId },
+        { $set: { status: req.body.status } },
         (err, order) => {
-            if(err){
+            if (err) {
                 return res.status(400).json({
                     error: "Cannot Update Order Status"
                 })
             };
             res.json(order);
-        };
+        }
     );
 };
