1bcedc7ca1c8a31f62bee69ef5e058363a8d61ab
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-19 00:23:20 +0530

    Basic frame that used in every page.

---
+++ projfrontend/src/core/Base.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Menu from "./Menu";
+
+
+const Base = ({
+    title = "My Title",
+    description = "My description",
+    className = "bg-dark text-white py-4 mb-1 text-center",
+    children
+}) => {
+    return (
+        <div>
+            <Menu />
+            <div className="container-fluid">
+                <div className="jumbotron bg-dark text-white text-center mb-3 py-4">
+                    <h2 className="diaplay-4">{title}</h2>
+                    <p className="lead">{description}</p>
+                </div>
+                <div className={className}>{children}</div>
+            </div>
+            <footer className="footer bg-dark mt-auto py-3">
+                <div className="container-fluid bg-success text-white text-center py-3">
+                    <h4>If you got any questions feel free to reach us</h4>
+                    <button className="btn btn-warning btn-lg">Contact US</button>
+                </div>
+                <div className="container text-center">
+                    <span className="text-muted">
+                        An Amazing <span className="text-white">MERN</span> BootCamp
+                    </span>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Base;
