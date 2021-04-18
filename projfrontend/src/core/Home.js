a59277972ce3aff4be21dcd0b1c75b70edcc72be
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-19 00:25:05 +0530

    Modified Home.js by adding Base page.

---
+++ projfrontend/src/core/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../styles.css";
+import { API } from "../backend";
+import Base from "./Base";
+
+const Home = () => {
+    console.log("API IS ", API);
+    return (
+        <Base>
+
+        </Base>
+    );
+};
+
+export default Home;
