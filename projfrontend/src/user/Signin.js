946c19e5673ae50f7d0523c587bd42e63fc157e6
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-23 02:04:30 +0530

    Created Signin page and form and methods for functioning.

---
+++ projfrontend/src/user/Signin.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import Base from "../core/Base";
+import { Link, Redirect } from "react-router-dom";
+import { authenticate, signin, isAuthenticated } from "../auth/helper"
+
+const Signin = () => {
+
+    const [values, setValues] = useState({
+        email: "",
+        password: "",
+        error: "",
+        loading: false,
+        didRedirect: false
+    });
+    const { email, password, error, loading, didRedirect } = values;
+    const { user } = isAuthenticated();
+
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value })
+    };
+
+    const onSubmit = event => {
+        event.preventDefault();
+        setValues({ ...values, error: false, loading: true })
+        signin({ email, password })
+            .then(data => {
+                if (data.error) {
+                    setValues({ ...values, error: data.error, loading: false });
+                } else {
+                    authenticate(data, () => {
+                        setValues({
+                            ...values,
+                            didRedirect: true
+                        });
+                    });
+                };
+            })
+            .catch(console.log("Error in Signin Form"));
+    };
+
+    const performRedirect = () => {
+        //TODO:
+        if (didRedirect) {
+            if (user && user.role === 1) {
+                return <p>Redirect to Admin!!!</p>
+            } else {
+                <p>Redirect to User!!!</p>
+            }
+        }
+        if (isAuthenticated()) {
+            return <Redirect to="/" />
+        }
+    }
+
+    const loadingMessage = () => {
+        return (
+            loading && (
+                <div className="alert alert-info">
+                    <h2>Loading...</h2>
+                </div>
+            )
+        );
+    }
+
+    const errorMessage = () => {
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <div className="alert alert-danger" style={{ display: error ? "" : "none" }}>
+                        {error}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const signInForm = () => {
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <form>
+                        <div className="from-group">
+                            <label className="text-light">Email</label>
+                            <input className="form-control" onChange={handleChange("email")} type="email" value={email} />
+                        </div>
+                        <div className="from-group">
+                            <label className="text-lightt">Password</label>
+                            <input className="form-control" onChange={handleChange("password")} type="password" value={password} />
+                        </div>
+                        <button onClick={onSubmit} className="btn btn-success btn-block mt-3">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <Base title="Signin Page" description="A page to Signin!!!">
+            {loadingMessage()}
+            {errorMessage()}
+            {signInForm()}
+            {performRedirect()}
+            <p className="text-white text-center">{JSON.stringify(values)}</p>
+        </Base>
+    );
+};
+
+export default Signin;
