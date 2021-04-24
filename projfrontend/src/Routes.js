a9e91851adaf5d3b3cad530637deedd7760b4ced
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-24 23:53:36 +0530

    Added private route, Admin routes.

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import Home from "./core/Home";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
+import UserDashBoard from "./user/UserDashBoard";
+import AdminDashBoard from "./user/AdminDashBoard";
+import AddCategory from "./admin/AddCategory";
+
+import AdminRoute from "./auth/helper/AdminRoutes";
+import PrivateRoute from './auth/helper/PrivateRoutes';
 
 const Routes = () => {
     return (
@@ -12,6 +19,9 @@ const Routes = () => {
                     <Route path="/" exact component={Home} />
                     <Route path="/signup" exact component={Signup} />
                     <Route path="/signin" exact component={Signin} />
+                    <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
+                    <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
+                    <AdminRoute path="/admin/create/category" exact component={AddCategory} />
                 </Switch>
             </BrowserRouter>
         </div>
