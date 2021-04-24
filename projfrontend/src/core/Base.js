358d00657c0769ee329b07c0e9d6ce9bb83422b5
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-25 00:09:49 +0530

    Modified Base.js by adding some inline styling.

--- projfrontend/src/core/Base.js
+++ projfrontend/src/core/Base.js
@@ -11,17 +11,17 @@ const Base = ({
     return (
         <div>
             <Menu />
-            <div className="container-fluid">
+            <div className="container">
                 <div className="jumbotron bg-dark text-white text-center mb-3 py-4">
                     <h2 className="diaplay-4">{title}</h2>
-                    <p className="lead">{description}</p>
+                    <p className="muted">{description}</p>
                 </div>
                 <div className={className}>{children}</div>
             </div>
             <footer className="footer bg-dark mt-auto py-3">
-                <div className="container-fluid bg-success text-white text-center py-3">
+                <div className="container-fluid text-white text-center py-3" style={{ backgroundColor: "#040B14" }}>
                     <h4>If you got any questions feel free to reach us</h4>
-                    <button className="btn btn-warning btn-lg">Contact US</button>
+                    <button className="btn btn-lg" style={{ backgroundColor: "#FFA500" }} >Contact US</button>
                 </div>
                 <div className="container text-center">
                     <span className="text-muted">
