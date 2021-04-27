b902b509b4cdcf0fe60c8fe13f5deaaa96417649
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-27 06:12:03 +0530

    Modified Routes.js by adding three more AdminRoutes for managing categories and for creating and managing products.

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -7,6 +7,9 @@ import Signin from "./user/Signin";
 import UserDashBoard from "./user/UserDashBoard";
 import AdminDashBoard from "./user/AdminDashBoard";
 import AddCategory from "./admin/AddCategory";
+import ManageCategories from "./admin/ManageCategories"
+import AddProduct from "./admin/AddProduct";
+import ManageProducts from './admin/ManageProducts';
 
 import AdminRoute from "./auth/helper/AdminRoutes";
 import PrivateRoute from './auth/helper/PrivateRoutes';
@@ -22,6 +25,10 @@ const Routes = () => {
                     <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
                     <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
                     <AdminRoute path="/admin/create/category" exact component={AddCategory} />
+                    <AdminRoute path="/admin/categories" exact component={ManageCategories} />
+                    <AdminRoute path="/admin/create/product" exact component={AddProduct} />
+                    <AdminRoute path="/admin/products" exact component={ManageProducts} />
+
                 </Switch>
             </BrowserRouter>
         </div>
