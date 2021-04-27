3c9185e38608d3f3b2deb5b9461e6c4f3e9ae8a0
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-27 06:09:15 +0530

    Modified adminapicall.js by adding some of the methods which fetch data from back end after making CRUD operations...

--- projfrontend/src/admin/helper/adminapicall.js
+++ projfrontend/src/admin/helper/adminapicall.js
@@ -1,5 +1,7 @@
 import { API } from "../../backend";
 
+//* Category Calls
+//* Create Category
 export const createCategory = (userId, token, categoryName) => {
     return fetch(`${API}/category/create/${userId}`, {
         method: "POST",
@@ -15,3 +17,84 @@ export const createCategory = (userId, token, categoryName) => {
         })
         .catch(error => console.log(error));
 };
+
+//* Get all categories
+export const getAllCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err))
+}
+
+//* Product Calls
+//* Create Product
+export const createProduct = (userId, token, product) => {
+    return fetch(`${API}/product/create/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    })
+        .then(response => {
+            return response.json()
+        })
+        .catch(err => console.log(err))
+}
+
+//* Get all Products
+export const getAllProducts = () => {
+    return fetch(`${API}/products`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err))
+}
+
+//* Get a Product
+export const getProduct = (productId) => {
+    return fetch(`${API}/product/${productId}`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json()
+        })
+        .catch(err => console.log(err))
+}
+
+//* Update a Product
+export const updateProduct = (productId, userId, token, product) => {
+    return fetch(`${API}/product/${productId}/${userId}`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    })
+        .then(response => {
+            return response.json()
+        })
+        .catch(err => console.log(err))
+}
+
+//* Delete a Product
+export const deleteProduct = (userId, token, productId) => {
+    return fetch(`${API}/product/${productId}/${userId}`, {
+        method: "DELETE",
+        headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json()
+        })
+        .catch(err => console.log(err))
+}
