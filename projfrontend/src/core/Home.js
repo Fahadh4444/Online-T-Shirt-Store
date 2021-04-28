3680a37743c26e627725fd22a9808c628356d5a5
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-28 22:56:28 +0530

    Updated Home.js by adding Cards to it.

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,13 +1,29 @@
 import React from 'react';
+
 import "../styles.css";
+
 import { API } from "../backend";
+
 import Base from "./Base";
+import Card from './Card';
 
 const Home = () => {
+
     console.log("API IS ", API);
+
     return (
         <Base title="Home Page" description="Welecome to T-Shirt Store">
-
+            <div className="row">
+                <div className="col-4">
+                    <Card ml="9px" />
+                </div>
+                <div className="col-4">
+                    <Card />
+                </div>
+                <div className="col-4">
+                    <Card mr="9px" />
+                </div>
+            </div>
         </Base>
     );
 };
