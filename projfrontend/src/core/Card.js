b1cae2598ee6729fe2d1c0886dbf994d9b4e204e
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-05-01 03:10:12 +0530

    Updated Card.js by modifying showAddtoCart, removeFromCart functions also added addToCart function.

--- projfrontend/src/core/Card.js
+++ projfrontend/src/core/Card.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Redirect } from "react-router-dom"
+
+import { addItemToCart, removeItemFromCart } from './helper/CartHelper';
 
 import ImageHelper from './helper/ImageHelper';
 
@@ -7,18 +10,33 @@ const Card = ({
     mr,
     product,
     addtoCart = true,
-    removeFromCart = false
+    removeFromCart = false,
+    setReload = f => f,
+    reload = undefined
 }) => {
+
+    const [redirect, setRedirect] = useState(false);
+    const [count, setCount] = useState(product.count)
 
     const cartTitle = product ? product.name : "A Photo from FF";
     const cartDescription = product ? product.description : "Default Description";
     const cartPrice = product ? product.price : "DEFAULT";
 
+    const addToCart = () => {
+        addItemToCart(product, () => setRedirect(true))
+    }
+
+    const getARedirect = (redirect) => {
+        if (redirect) {
+            return <Redirect to="/cart" />
+        }
+    }
+
     const showAddtoCart = (addtoCart) => {
         return (
             addtoCart && (
                 <button
-                    onClick={() => { }}
+                    onClick={addToCart}
                     className="btn btn-block btn-outline-success mt-2 mb-2"
                 >
                     Add to Cart
@@ -31,7 +49,10 @@ const Card = ({
         return (
             removeFromCart && (
                 <button
-                    onClick={() => { }}
+                    onClick={() => {
+                        removeItemFromCart(product._id);
+                        setReload(!reload)
+                    }}
                     className="btn btn-block btn-outline-danger mt-2 mb-2"
                 >
                     Remove from cart
@@ -44,6 +65,7 @@ const Card = ({
         <div className="card text-white bg-dark border  " style={{ marginLeft: ml, marginRight: mr }}>
             <div className="card-header lead">{cartTitle}</div>
             <div className="card-body">
+                {getARedirect(redirect)}
                 <ImageHelper product={product} />
                 <p className="lead bg-warning font-weight-normal text-wrap" style={{ color: "black", fontWeight: "450" }}>
                     {cartDescription}
