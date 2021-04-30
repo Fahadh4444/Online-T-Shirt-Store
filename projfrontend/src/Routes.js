af615ad71d5a9a1b4d824bf9fadc0715e0826241
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-05-01 03:05:58 +0530

    Updated Routes.js by adding cart page route.

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -12,6 +12,7 @@ import AddProduct from "./admin/AddProduct";
 import ManageProducts from './admin/ManageProducts';
 import UpdateProduct from './admin/UpdateProduct';
 import UpdateCategories from './admin/UpdateCategories';
+import Cart from './core/Cart';
 
 import AdminRoute from "./auth/helper/AdminRoutes";
 import PrivateRoute from './auth/helper/PrivateRoutes';
@@ -24,6 +25,7 @@ const Routes = () => {
                     <Route path="/" exact component={Home} />
                     <Route path="/signup" exact component={Signup} />
                     <Route path="/signin" exact component={Signin} />
+                    <Route path="/cart" exact component={Cart} />
                     <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
                     <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
                     <AdminRoute path="/admin/create/category" exact component={AddCategory} />
