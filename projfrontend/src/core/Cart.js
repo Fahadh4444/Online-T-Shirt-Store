3efc08b07fc3d1f0c1bdf69acf459d298cbfc59e
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-05-01 03:08:23 +0530

    Created Cart page and loaded all products in cart to cart page.

---
+++ projfrontend/src/core/Cart.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+
+import "../styles.css";
+
+import { API } from "../backend";
+
+import Base from "./Base";
+import Card from './Card';
+
+import { loadCart } from './helper/CartHelper';
+
+
+const Cart = () => {
+
+    const [products, setProducts] = useState([]);
+    const [reload, setReload] = useState(false);
+
+    useEffect(() => {
+        setProducts(loadCart())
+    }, [reload])
+
+    const loadAllProducts = () => {
+        return (
+            <div>
+                <h2>
+                    This Section is to Load Products
+                </h2>
+                {products.map((product, index) => (
+                    <Card
+                        key={index}
+                        product={product}
+                        ml="18px"
+                        addtoCart={false}
+                        removeFromCart={true}
+                        setReload={setReload}
+                        reload={reload}
+                    />
+                ))}
+            </div>
+        )
+    }
+
+    const loadCheckOut = () => {
+        return (
+            <div>
+                <h2>
+                    This Section is for Check out
+                </h2>
+            </div>
+        )
+    }
+
+    return (
+        <Base title="Cart Page" description="Read to Check Out">
+            <div className="row">
+                <div className="col-6">
+                    {loadAllProducts()}
+                </div>
+                <div className="col-6">
+                    {loadCheckOut()}
+                </div>
+            </div>
+        </Base>
+    );
+};
+
+export default Cart;
