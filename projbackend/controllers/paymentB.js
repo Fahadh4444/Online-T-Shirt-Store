5e2977833e65ca6804740189ef3a5c77425e03e3
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-07-13 23:53:40 +0530

    Updated paymentB.js in conttroller by adding orginal Id and keys for BraintreeGateway.

--- projbackend/controllers/paymentB.js
+++ projbackend/controllers/paymentB.js
@@ -2,9 +2,9 @@ const braintree = require("braintree");
 
 const gateway = new braintree.BraintreeGateway({
     environment: braintree.Environment.Sandbox,
-    merchantId: "useYourMerchantId",
-    publicKey: "useYourPublicKey",
-    privateKey: "useYourPrivateKey"
+    merchantId: "3g388rgnrjkgftwb",
+    publicKey: "mbnzt3g7mg2sjw4t",
+    privateKey: "52e03863d793b79179e3cdaccf32c3c2"
 });
 
 //* getToken Route Method
@@ -12,9 +12,9 @@ exports.getToken = (req, res) => {
     gateway.clientToken.generate({}, (err, response) => {
         // pass clientToken to your front-end
         if (err) {
-            res.status(500).json(err)
+            res.status(400).json(err)
         } else {
-            res.json(response)
+            res.send(response)
         }
     });
 };
